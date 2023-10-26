1ff2f865d9a84ffa37bec641c5962a8510cf91cf
Egor <145016046+Infinity164@users.noreply.github.com>
2023-10-26 14:21:47 +0300

    create guess the number console game

---
+++ guess_the_number.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int player, computer, c_c, c_p;
+	c_c = 0, c_p = 0;
+	computer = 1 + rand() % 101;
+	cout << "Компьютер загадал число в диапазоне от 1 до 100" << endl;
+	for (int count = 0; ; ++count) {
+		cin >> player;
+		if (player < computer) {
+			cout << "Загаданное число больше вашего." << endl;
+		}
+		else if (player > computer) {
+			cout << "Загаданное число меньше вашего." << endl;
+		}
+		else if (player == computer) {
+			cout << "Вы угадали число! Очко игроку." << endl;
+			c_p++;
+			computer = 1 + rand() % 101;
+			count = 0;
+			continue;
+		}
+		if (count >= 5) {
+			cout << "Вы не отгадали за 5 попыток. Очко компу." << endl;
+			c_c++;
+			computer = 1 + rand() % 101;
+			count = 0;
+			continue;
+		}
+		if (c_p == 3 || c_c == 3) {
+			cout << "Игра окончена!" << endl << "Итоговый счет: " << c_p << " игрок/компьютер " << c_c;
+			break;
+		}
+	}
+}
