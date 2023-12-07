f46f9c800369c6e78dbca7ac9fa61022b77f41c0
Egor <145016046+Infinity164@users.noreply.github.com>
2023-12-07 12:51:51 +0300

    Add files via upload

---
+++ mmarray.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "rus");
+
+    const int num = 10;
+    int enda;
+    int summ = 0;
+    int minIndex = 0;
+    int maxIndex = 0;
+    cin >> enda;
+    int array[num] = {};
+    for (int i = 0; i < num; i++) {
+        array[i] = 1 + rand() % enda;
+        cout << array[i] << " ";
+    }
+    int max = array[0];
+    int min = array[0];
+    for (int i = 0; i < num; i++) {
+        if (max < array[i]) {
+            max = array[i];
+            maxIndex = i;
+        }
+        if (min > array[i]) {
+            min = array[i];
+            minIndex = i;
+        }
+    }
+    cout << endl << min << " " << minIndex;
+    cout << endl << max << " " << maxIndex;
+}
+
