cd7a13367823f48898c6f09005b21233a3a418aa
Egor <145016046+Infinity164@users.noreply.github.com>
2024-06-05 13:41:12 +0300

    Add files via upload

---
+++ singleton.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class ConfigManager {
+    static ConfigManager* instance;
+    string key;
+    string value;
+    map<string, string> parameters;
+    ConfigManager(){}
+public:
+    static ConfigManager* getInstance() {
+        if (instance == nullptr) { instance = new ConfigManager(); }
+        return instance;
+    }
+    void setParameter(string key, string value) { parameters[key] = value; }
+    string getParameter(string key) { return parameters[key]; }
+};
+
+ConfigManager* ConfigManager::instance = nullptr;
+
+int main()
+{
+   
+}
+
