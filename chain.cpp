a55118ecaf698ffb89799620c11e52e78e9b7cf8
Egor <145016046+Infinity164@users.noreply.github.com>
2024-06-18 10:57:42 +0300

    Add files via upload

---
+++ chain.cpp
@@ -0,0 +1,51 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class BaseHandler {
+protected:
+    BaseHandler* next;
+public:
+    BaseHandler() : next(nullptr){}
+    void setNext(BaseHandler* nextHandler) { next = nextHandler; }
+    virtual void handleRequest(string request) { 
+        if (next) next->handleRequest(request);
+        else throw logic_error("Request not recognized.");
+    }
+};
+
+class SimpleRequestHandler : public BaseHandler {
+public:
+    void handleRequest(string request) {
+        if (request == "simple") cout << "Handled the request (simple)" << endl;
+        else BaseHandler::handleRequest(request);
+    }
+};
+
+class ComplexRequestHandler : public BaseHandler {
+public:
+    void handleRequest(string request) {
+        if (request == "complex") cout << "Handled the request (complex)" << endl;
+        else BaseHandler::handleRequest(request);
+    }
+};
+
+class CriticalRequestHandler : public BaseHandler {
+public:
+    void handleRequest(string request) {
+        if (request == "critical") cout << "Handled the request (critical)" << endl;
+        else BaseHandler::handleRequest(request);
+    }
+};
+
+int main()
+{
+    SimpleRequestHandler* sreqh = new SimpleRequestHandler();
+    ComplexRequestHandler* creqh = new ComplexRequestHandler();
+    CriticalRequestHandler* crreqh = new CriticalRequestHandler();
+
+    sreqh->setNext(creqh);
+    creqh->setNext(crreqh);
+    sreqh->handleRequest("critical");
+}
