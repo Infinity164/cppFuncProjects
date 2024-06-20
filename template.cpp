67ec564a28c3f3fb53e81792e25b761f702e3218
Egor <145016046+Infinity164@users.noreply.github.com>
2024-06-20 10:54:20 +0300

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Coffee {
+public:
+    void prepareRecipe() {
+        boilWater();
+        brewCoffee();
+        pourInCup();
+        addOptionalCondiments();
+    }
+protected:
+    void boilWater() { cout << "Boiling water" << endl; }
+    void pourInCup() { cout << "Pouring in cup" << endl; }
+
+    virtual void brewCoffee() = 0;
+    virtual void addOptionalCondiments() = 0;
+};
+
+class BlackCoffee : public Coffee {
+protected:
+    void brewCoffee() override { cout << "Dripping black coffee" << endl; }
+    void addOptionalCondiments() override { cout << "Nothing to add, sorry" << endl; }
+};
+
+class Latte : public Coffee {
+protected:
+    void brewCoffee() override { cout << "Brewing espresso" << endl; }
+    void addOptionalCondiments() override { cout << "Adding some milk" << endl; }
+};
+
+int main()
+{
+    
+}
+
+
